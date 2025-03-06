d243bde3434cf4a40513a20f22bef987d6047127
jaron <jaron@jaronnie.com>
2025-03-06 13:44:09 +0800

    feat(grum): add version cmd

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ package main
 
 import "github.com/jaronnie/grum/cmd"
 
+// ldflags
+var (
+	version = "1.3.0"
+	commit  string
+	date    string
+)
+
 func main() {
+	cmd.Version = version
+	cmd.Date = date
+	cmd.Commit = commit
+
 	cmd.Execute()
 }
